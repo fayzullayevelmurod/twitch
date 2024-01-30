b3af40e658606743a2ff2b99dd8a09b9d47d8a67
QobilovSardor <note47297@gmail.com>
2024-01-30 15:05:57 +0500

    first commit

---
+++ src/app.tsx
@@ -0,0 +1,10 @@
+export const App = () => {
+	return (
+		<div className="Group8 w-96 h-96 relative">
+			<div className="MaskGroup w-96 h-96 left-0 top-0 absolute">
+			</div>
+			<div className=" w-64 left-[47px] top-[108px] absolute text-zinc-100 text-xl font-normal font-['Gilroy'] leading-9">100% соответствие реальным зрителям. Наши зрители просматривают рекламу, участвуют в голосованиях, прогнозах <br />и рейдах, пишут в чат и подписываются на каналы</div>
+			<div className=" left-[47px] top-[45px] absolute text-zinc-100 text-xl font-normal font-['2MASS J1808'] leading-10">Соответствие</div>
+		</div>
+	)
+}
